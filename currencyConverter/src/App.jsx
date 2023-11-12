147b90ec5a9534e3a026a8a67daa602899e4287e
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2023-11-12 19:16:45 +0530

    added

---
+++ currencyConverter/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl bg-orange-500">Currrency app with chai</h1>
+    </>
+  );
+}
+
+export default App;
